a7cfc8905522224ea7213fc7832731a8a1b5f0ee
RayaneF03 <rayane.f.santos7@aluno.senai.br>
2026-06-01 16:02:07 -0300

    fix: Corrigir inicialização do cliente Groq e ajustar formatação da requisição

--- Aula-03/exemple_jupyter.py
+++ Aula-03/exemple_jupyter.py
@@ -1,20 +1,21 @@
 from groq import Groq
 import pandas as pd
 
-Client = Groq (
-    api_key = "YOUR_API_KEY")
+client = Groq (
+    api_key = "QdNeKQq9rxPJN6bCHnJDqV8tPh3ll98b"
+    )
 
 response = client.chat.completions.create(
-    model = "openai/gpt-oss-120b"),
-messages = [
-    {
-        "role": "user",
-        "content": "Liste as 5 linguagens utilizadas em IA."
+    model = "openai/gpt-oss-120b",
+    messages = [
+        {
+            "role": "user",
+            "content": "Liste as 5 linguagens utilizadas em IA."
         }
-        ]
+    ]
 )
 
-texto = response.choise[0].message.content
+texto = response.choices[0].message.content
 
 df = pd.DataFrame({
     "Resposta": [texto]
