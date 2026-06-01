fd555747a4efadf5a898cdbaad31c70929b037b2
RayaneF03 <rayane.f.santos7@aluno.senai.br>
2026-06-01 16:42:08 -0300

    fix: problems with API Key

--- Aula-03/exemple_jupyter.py
+++ Aula-03/exemple_jupyter.py
@@ -1,9 +1,9 @@
 from groq import Groq
 import pandas as pd
 
-client = Groq (
-    api_key = "QdNeKQq9rxPJN6bCHnJDqV8tPh3ll98b"
-    )
+client = Groq(
+    api_key = "APIAI"
+)
 
 response = client.chat.completions.create(
     model = "openai/gpt-oss-120b",
@@ -12,11 +12,13 @@ response = client.chat.completions.create(
             "role": "user",
             "content": "Liste as 5 linguagens utilizadas em IA."
         }
-    ]
+    ] 
 )
 
 texto = response.choices[0].message.content
 
 df = pd.DataFrame({
     "Resposta": [texto]
-    })
+})
+
+print(texto)
